e59c1f0a627efb6b2d7b754c0f769313923dd20b
RockybDeath <45093728+RockybDeath@users.noreply.github.com>
2020-07-28 09:37:16 +0300

    Add files via upload

---
+++ build/precache-manifest.cd05635fae0b4dc98401411432b9d6ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a5dc70b0f367ed1e2cfc0a51326425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9804bd2ca221ea6ec7ea",
+    "url": "/static/css/main.0a3fceb4.chunk.css"
+  },
+  {
+    "revision": "b32f9e4c82987fa5f2fd",
+    "url": "/static/js/2.98a0066c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.98a0066c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9804bd2ca221ea6ec7ea",
+    "url": "/static/js/main.b1397e36.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
